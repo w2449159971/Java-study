9c255929ecdd9275a639bbaf1378c623daa7ecf9
leah4 <544700101@qq.com>
2019-09-19 22:58:23 +0800

    链表逆置

---
+++ ReverseList.java
@@ -0,0 +1,56 @@
+﻿
+class Node{
+    public int val;
+    public Node next;
+
+    public Node(int val){
+        this.val = val;
+        this.next = null;
+    }
+}
+public class ReverseList {
+    public static Node reverse(Node head){
+        Node newhead = null;
+        Node cur = head;
+        while(cur != null){
+            Node next = cur.next;
+            cur.next = newhead;
+            newhead = cur;
+            cur = next;
+        }
+        return newhead;
+    }
+
+    public static void print(Node head){
+        Node cur = head;
+        while (cur != null){
+            if(cur.next == null){
+                System.out.printf("%d ",cur.val);
+            }
+            else{
+                System.out.printf("%d ->",cur.val);
+            }
+            cur = cur.next;
+        }
+        System.out.println( );
+    }
+
+    public static Node creatlist(){
+        Node n1 = new Node(1);
+        Node n2 = new Node(2);
+        Node n3 = new Node(3);
+        Node n4 = new Node(4);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        return n1;
+    }
+
+    public  static void main(String[] args){
+        Node list = creatlist();
+        print(list);
+        Node newlist = reverse(list);
+        print(newlist);
+    }
+}
