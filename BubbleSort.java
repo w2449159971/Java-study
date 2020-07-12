b748dd84683af71757b15182c56caa6f06530ed9
leah4 <50786472+leah4@users.noreply.github.com>
2020-07-12 21:12:41 +0800

    冒泡排序

--- BubbleSort.java
+++ BubbleSort.java
@@ -15,10 +15,26 @@ class BubbleSort{
             }
         }
   }
-    private static void swap(int[] array, int j, int i) {
+  private static void swap(int[] array, int j, int i) {
         int tmp = array[i];
         array[i] = array[j];
         array[j] = tmp;
+   }
+    
+    //冒泡排序的基本实现
+    public static void bubbleSortNormal(int[] array){
+        int i,j;
+        for(i=0;i<array.length-1;i++)
+        {
+            for(j=0;j<array.length-1-i;j++)
+            {
+                if(array[j]>array[j+1])
+                {
+                    int temp=array[j];
+                    array[j]=array[j+1];
+                    array[j+1]=temp;
+                }
+            }
+        }
     }
-
 }
